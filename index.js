77d2ce2158585ef3e365b85e1fb25ab7d0fcb695
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-09-18 22:26:26 +0500

    Sum Two Number

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+function sum(num1, num2) {
+    //num1:number = 10;
+    //num2:number = 20;
+    var result = num1 + num2;
+    console.log("The sum of num1 and num2 is = ", result);
+}
+// sum of 2 numbers 10 and 20
+sum(10, 20);
+// sum of 2 numbers 30 and 40
+sum(30, 40);
---
+++ index.ts
@@ -0,0 +1,11 @@
+function sum(num1: number, num2: number) {
+    //num1:number = 10;
+    //num2:number = 20;
+    var result: number = num1 + num2;
+    console.log("The sum of num1 and num2 is = ", result);
+}
+// sum of 2 numbers 10 and 20
+sum(10, 20)
+
+// sum of 2 numbers 30 and 40
+sum(30, 40)
